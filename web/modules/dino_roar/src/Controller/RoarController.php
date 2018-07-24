131a53ef1756f2725803cb95dc53d63c74effa4e
hamdle <contact@ericmarty.co>
2018-07-23 22:32:48 -0700

    Get dino service from controller

--- web/modules/dino_roar/src/Controller/RoarController.php
+++ web/modules/dino_roar/src/Controller/RoarController.php
@@ -2,16 +2,31 @@
 
 namespace Drupal\dino_roar\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class RoarController
+class RoarController extends ControllerBase
 {
+  private $roarGenerator;
+
+  public function __construct(RoarGenerator $roarGenerator)
+  {
+    $this->roarGenerator = $roarGenerator;
+  }
+
   public function roar($count)
   {
-    $roarGen = new RoarGenerator();
-    $roar = $roarGen->getRoar($count);
+    $roar = $this->roarGenerator->getRoar($count);
 
     return new Response($roar);
+  }
+
+  public static function create(ContainerInterface $container)
+  {
+    $roarGenerator = $container->get('dino_roar.roar_generator');
+
+    return new static ($roarGenerator);
   }
 }
